dd900c62e677768688d2aa0ff6047c54aff3b0d4
Sidharth Balakrishnan <sidharth.balakrishnan@outlook.com>
2024-11-02 18:11:14 +0530

    profile details

--- app/(protected)/settings/index.tsx
+++ app/(protected)/settings/index.tsx
@@ -1,7 +1,9 @@
-import { View, Text, Touchable, TouchableOpacity } from 'react-native'
+import { View, Text, Touchable, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import { useAuth } from '@/hooks/useAuth';
 import { Link, router } from 'expo-router';
+import { Colors } from '@/constants/Colors';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
 
 export default function index() {
 
@@ -17,13 +19,75 @@ export default function index() {
   }
 
   return (
-    <View style={{ paddingTop: 150 }}>
-      <Text>Settings Landing Page</Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
-
-      <Text style={{ color: '#000', fontWeight: 'bold' }} onPress={() => router.navigate("/settings/profile")}>Profile Edit</Text>
+    <View>
+      
+      <View style={styles.titleContainer}>
+          <Text style={styles.title}>Your Profile</Text>
+      </View>
+      <View style={styles.profileContainer}>
+        <Ionicons name="person-circle" size={80} color={Colors.Primary} style={styles.profileImage}/> 
+        <View style={styles.profileInfo}>
+            <Text style={styles.profileTitle}>Sidharth Balakrishnan</Text>
+            <Text style={styles.profileSubtitle} onPress={() => router.navigate("/settings/profile")}>Edit Profile</Text>
+        </View>        
+      </View>      
+      <View style={styles.logoutButton}>
+        <TouchableOpacity onPress={() => handleLogout()}>
+          <Text style={styles.logoutButtonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop:20
+  },
+  title: {
+    fontSize:20,
+    fontFamily: 'outfit-bold'
+    },
+  profileContainer: {
+    margin: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    flexDirection: 'row'
+  },
+  profileImage: {
+      width: 80,
+      height: 80,
+      borderRadius:15
+  },
+  profileInfo: {
+      marginTop: 7,
+      marginLeft: 10
+  },
+  profileTitle: {
+      fontFamily: 'outfit-medium',
+      fontSize: 18
+  },
+  profileSubtitle: {
+      fontFamily: 'outfit',
+      fontSize: 14,
+      color: Colors.LightGrey
+  },
+  logoutButton: {
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 20,
+    padding:10,
+    backgroundColor: Colors.Primary,
+    justifyContent: "center", 
+    alignItems: "center"
+  },
+  logoutButtonText: {
+    fontFamily: 'outfit',
+    color: Colors.LighterGrey
+  }
+})
