cc2ff516c949f3268c1790e04a4beb07c11c0836
Sidharth Balakrishnan <sidharth.balakrishnan@outlook.com>
2024-11-19 18:04:40 +0530

    updated restaurant menu

--- app/(protected)/store/[id].tsx
+++ app/(protected)/store/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, ScrollView, TextInput, SectionList } from 'react-native'
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, ScrollView, TextInput, SectionList, ListRenderItem, ImageBackground } from 'react-native'
 import React, { useState } from 'react'
 import { router, useFocusEffect, useLocalSearchParams } from 'expo-router';
 import axios from 'axios';
@@ -99,72 +99,76 @@ export default function StoreDetails() {
       }, [])
     );
 
-    return (
-      <SafeAreaView style={{flex:1}}>
-        <ScrollView stickyHeaderIndices={[1]}>
-          <View style={styles.restaurantcontainer}>
-              <View style={styles.restaurantCard} >
-                <Text style={styles.restaurantTitle}>{restaurant.name}</Text>
-                <Text style={styles.restaurantSubtitle}>{restaurant.locality} • {restaurant.city} • 0.22 kms</Text>
-                <Text style={styles.restaurantSubtitle}>{restaurant.cuisine}</Text>
-              </View>
+    const renderItem: ListRenderItem<any> = ({item, index}) => (
+      <View style={{backgroundColor: Colors.White, padding: 10, flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            <Text style={{fontFamily: 'nunito-bold'}}>{item.name}</Text>
+            <Text style={{fontFamily: 'nunito-medium', color: Colors.LightGrey}}>Flavourful biriyani with a twist of chilli and salty chicken fry</Text>
+            <Text style={{fontFamily: 'nunito-medium', color: Colors.LightGrey}}>₹{item.price}</Text>
           </View>
           <View>
-            <View style={styles.searchContainer}>
-              <View style={styles.searchTextInputContainer}>
-                <Ionicons name="search" size={30} color={Colors.Primary} /> 
-                <TextInput style={styles.searchTextInput} placeholderTextColor={Colors.LightGrey} placeholder='Search' value={search} onChangeText={(text: string) => {setSearch(text); loadRestaurantMenu(text);}} ></TextInput>
-              </View>
+            <Image source={{uri:item.photo}} style={styles.foodImage} />
+            <View style={[styles.cartButtonContainer]}>
+              <TouchableOpacity onPress={() => handleRemoveItem('1', restaurant.id, item.id)}>
+                  <Text style={styles.cartButton}><Ionicons name="remove-sharp" size={24} color="{color}" /></Text>
+              </TouchableOpacity>
+              <Text style={styles.cartButtonText}>{item.quantity}</Text>
+              <TouchableOpacity onPress={() => handleAddItem('1', restaurant.id, item.id)}>
+                  <Text style={styles.cartButton}><Ionicons name="add-sharp" size={24} color="{color}" /></Text>
+              </TouchableOpacity>
             </View>
           </View>
-          <SectionList            
+      </View>
+    );
+
+    return (
+      <SafeAreaView style={{flex:1}}>
+          <SectionList 
+            keyExtractor={(item, index) => `${item.id + index}`} 
+            showsVerticalScrollIndicator={false}
             sections={restaurantMenu}
-            renderSectionHeader={({section}) => (
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => <View style={{height: 1, backgroundColor: Colors.LighterGrey}} />}
+            SectionSeparatorComponent={() => <View style={{height: 1, backgroundColor: Colors.LighterGrey}} />}
+            renderSectionHeader={({section: {title, index}}) => (
               <Text style={{
                 fontFamily: 'outfit-bold',
-                padding: 20,
-                fontSize: 18
-              }}>{section.title}</Text>
+                padding: 10,
+                fontSize: 18,
+                backgroundColor: 'white'
+              }}>{title}</Text>
             )}
-            renderItem={({item}) =>(
-              <View style={styles.foodContainer}>
-                  <View>
-                      <Text style={styles.foodTitle}>{item.name}</Text>
-                      <Text style={styles.foodSubtitle}>Flavourful biriyani with a twist of chilli and salty chicken fry</Text>
-                      <Text style={styles.foodSubtitle}>₹ {item.price}</Text>
-                  </View>
-                  <View>
-                      <Image source={{uri:item.photo}} style={styles.foodImage} />
-                      <View style={[styles.cartButtonContainer, styles.cartButtonWPContainer]}>
-                          <TouchableOpacity onPress={() => handleRemoveItem('1', restaurant.id, item.id)}>
-                              <Text style={styles.cartButton}><Ionicons name="remove-sharp" size={24} color="{color}" /></Text>
-                          </TouchableOpacity>
-                          <Text style={styles.cartButtonText}>{item.quantity}</Text>
-                          <TouchableOpacity onPress={() => handleAddItem('1', restaurant.id, item.id)}>
-                              <Text style={styles.cartButton}><Ionicons name="add-sharp" size={24} color="{color}" /></Text>
-                          </TouchableOpacity>
-                      </View>
-                  </View>
+            ListHeaderComponent={() => (
+              <View style={styles.restaurantcontainer}>
+                <View style={styles.restaurantCard} >
+                  <Text style={styles.restaurantTitle}>{restaurant.name}</Text>
+                  <Text style={styles.restaurantSubtitle}>{restaurant.locality} • {restaurant.city} • 0.22 kms</Text>
+                  <Text style={styles.restaurantSubtitle}>{restaurant.cuisine}</Text>
+                </View>
               </View>
             )}
-            scrollEnabled={false}
-            keyExtractor={item => `${item.id}`}
+            stickySectionHeadersEnabled={true}
           />
-        </ScrollView>
-        {cartValue.quantity > 0 ? (
-          <View style={styles.checkoutButton}>
-            <TouchableOpacity onPress={() => router.push('/cart')}>
-              <Text style={styles.checkoutButtonText}>{cartValue.quantity} item{cartValue.quantity > 1 ? 's': null} added</Text>
-            </TouchableOpacity>
+          <View style={styles.searchContainer}>
+            <View style={styles.searchTextInputContainer}>
+              <Ionicons name="search" size={30} color={Colors.Primary} /> 
+              <TextInput style={styles.searchTextInput} placeholderTextColor={Colors.LightGrey} placeholder='Search' value={search} onChangeText={(text: string) => {setSearch(text); loadRestaurantMenu(text);}} ></TextInput>
+            </View>
           </View>
-        ) : null}
+          {cartValue.quantity > 0 ? (
+            <View style={styles.checkoutButton}>
+              <TouchableOpacity onPress={() => router.push('/cart')}>
+                <Text style={styles.checkoutButtonText}>{cartValue.quantity} item{cartValue.quantity > 1 ? 's': null} added</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null}
       </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
   restaurantcontainer: {
-    backgroundColor: Colors.Primary,
+    backgroundColor: Colors.Tertiary,
     paddingVertical: 10,
   },
   restaurantCard: {
@@ -185,7 +189,6 @@ const styles = StyleSheet.create({
   searchContainer: {
     backgroundColor: Colors.White,
     paddingHorizontal: 10,
-    marginBottom: 10,
   },
   searchTextInputContainer: { 
     padding: 10,
@@ -223,12 +226,24 @@ const styles = StyleSheet.create({
       color: Colors.LightGrey
   },
   foodImage: {
-      width: 130,
-      height: 100,
-      marginLeft:0,
-      borderTopLeftRadius: 15,
-      borderTopRightRadius: 15
+      width: 120,
+      height: 120,
+      borderTopLeftRadius:15,
+      borderTopRightRadius:15
   },
+  checkoutButton: {
+    padding:20,
+    height: 70,
+    backgroundColor: Colors.Primary, justifyContent: "center", alignItems: "center"
+  },
+  checkoutButtonText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
+    color: Colors.Tertiary,
+  },
+
+
+  
   cartButtonContainer: {
     display: 'flex',
     flexDirection: 'row',
@@ -237,16 +252,8 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.Tertiary, 
     paddingLeft: 15, 
     paddingRight: 15,
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 15
-  },
-  cartButtonWOPContainer: {
-    width: 130,
-    borderRadius: 15
-  },
-  cartButtonWPContainer: {
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 15
+    borderBottomLeftRadius:15,
+    borderBottomRightRadius:15
   },
   cartButton: {
     fontFamily: 'nunito-medium',
@@ -259,14 +266,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: Colors.Primary
   },
-  checkoutButton: {
-    padding:20,
-    height: 70,
-    backgroundColor: Colors.Primary, justifyContent: "center", alignItems: "center"
-  },
-  checkoutButtonText: {
-    fontFamily: 'nunito-bold',
-    fontSize: 18,
-    color: Colors.Tertiary,
-  }
 })
