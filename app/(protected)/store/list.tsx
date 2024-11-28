c9605b058bde38e2a9ca89797690f859d0cf54d2
Sidharth Balakrishnan <sidharth.balakrishnan@outlook.com>
2024-11-28 18:28:43 +0530

    search functionalites

--- app/(protected)/store/list.tsx
+++ app/(protected)/store/list.tsx
@@ -1,15 +1,17 @@
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Colors } from '@/constants/Colors';
 import RestaurantNearbyCard from '@/components/store/RestaurantNearbyCard';
 import {API_URL} from '@env';
 import { useNavigation } from 'expo-router';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
 
 export default function list() {
   const [isLoading, setIsLoading] = useState(true);
   const [restaurants, setRestaurants] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
   const navigation = useNavigation();
   
   useEffect(() => {
@@ -19,11 +21,25 @@ export default function list() {
   const loadRecentRestaurants = async() => {
     try {
       setIsLoading(true);
-      const response = await axios.get(`${API_URL}/restaurant/list?page=${currentPage}&pageSize=10`);
+      const response = await axios.get(`${API_URL}/restaurant/list?page=${currentPage}&pageSize=10&query=${search}`);
       if (response.data.length > 0) {
         setRestaurants((items) => items.concat(response.data));
         setCurrentPage(currentPage + 1);
       }
+      setIsLoading(false);
+    }
+    catch(error) {
+      console.log(error);
+    } 
+  }
+
+  
+  const searchRecentRestaurants = async(searchText: string) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`${API_URL}/restaurant/list?page=1&pageSize=10&query=${searchText}`);
+      setRestaurants(response.data);
+      setCurrentPage(2);
       setIsLoading(false);
     }
     catch(error) {
@@ -37,45 +53,86 @@ export default function list() {
     }
   }
 
-  const listHeaderComponent = () => {
-      return (        
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}># Nearby Hotspots</Text>
-        </View>
-      );
-  }
-
   const listFooterComponent = () => {
-      return (
-        <Text style={styles.footer}>You've reached the end</Text>
-      );
+    return (
+      <Text style={styles.footer}>You've reached the end</Text>
+    );
   }
 
   useEffect(() => {
-      loadRecentRestaurants();
+    loadRecentRestaurants();
   }, []);
 
   return (
-    <View style={{paddingHorizontal:10, flex:1}}>
+    <View style={{flex:1}}>
       <FlatList data={restaurants}
-                renderItem={({item, index})=>(<RestaurantNearbyCard restaurant={item} key={index} />)}
+                stickyHeaderIndices={[0]}
+                renderItem={({item, index})=>(
+                  <View style={{paddingHorizontal: 10}}>
+                    <RestaurantNearbyCard restaurant={item} key={index} />
+                  </View>
+                )}
                 keyExtractor={(item, index) => String(index)}
                 showsVerticalScrollIndicator={false}
                 onEndReached={onEndReached}
                 onEndReachedThreshold={0.5}
-                ListHeaderComponent={listHeaderComponent}
+                ListHeaderComponent={
+                  <View>        
+                    <View style={searchStyles.searchContainer}>
+                      <View style={searchStyles.searchTextInputContainer}>
+                        <Ionicons name="search" size={20} style={{paddingTop:13}} color={Colors.LightGrey} /> 
+                        <TextInput style={searchStyles.searchTextInput} 
+                                  placeholderTextColor={Colors.LightGrey} 
+                                  placeholder='Search' 
+                                  value={search} 
+                                  onChangeText={(text: string) => {
+                                    setSearch(text); 
+                                    searchRecentRestaurants(text);
+                                  }}
+                        />
+                      </View>
+                    </View>
+                    <View style={styles.titleContainer}>
+                      <Ionicons name="location-sharp" size={24} color={Colors.Primary} /> 
+                      <Text style={styles.title}>Nearby Hotspots</Text>
+                    </View>
+                  </View> 
+                }
                 ListFooterComponent={listFooterComponent}
       />
     </View>
   )
 }
 
+const searchStyles = StyleSheet.create({
+  searchContainer: {
+    backgroundColor: Colors.White,
+    paddingHorizontal: 10,
+  },
+  searchTextInputContainer: { 
+    padding: 10,
+    marginVertical: 10,
+    flexDirection: 'row',
+    gap:10,
+    borderRadius: 10,
+    backgroundColor: Colors.LighterGrey,
+  },
+  searchTextInput: {
+    flex:1,
+    fontSize: 20,
+    paddingRight:40
+  }  
+});
+
 const styles = StyleSheet.create({
   titleContainer: {
-    marginVertical: 10,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: Colors.White,
+    gap: 5,
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize:20,
